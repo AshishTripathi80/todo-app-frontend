246091125d75b9779bcadb7d24d16d0ea59a404f
Ashish Tripathi <tripashi6@gmail.com>
2024-02-02 16:04:15 +0530

    Final Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import TaskList from "./pages/TaskList";
+import Task from "./components/Task";
+import AddTask from "./components/AddTask";
+import EditTask from "./components/EditTask";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<TaskList />} />
+        <Route path="/task/:taskId" element={<Task />} />
+        <Route path="/add/task" element={<AddTask />} />
+        <Route path="/edit/:taskId" element={<EditTask/>}/>
+        {/* Add other routes as needed */}
+      </Routes>
+    </Router>
   );
 }
 
